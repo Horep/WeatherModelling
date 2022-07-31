00e332f33de7591ca82d6fa5cee0c5cc879be859
Horep <horep245@gmail.com>
2022-08-01 00:15:23 +0100

    more pep8

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ FrostDays = pull_data('FrostDays')
 Rain = pull_data('Rain')
 SunHours = pull_data('SunHours')
 
+
 def calculate_coefficients(t, data):
     #  modelling as (a + b*t + c*cos(wt) + d*sin(wt))
     #  determine x=[a,b,c,d] from Ax=b, where b is known, and A comes from the model above
@@ -39,8 +40,8 @@ def plot_data(t, data, name):
     fig, ax = plt.subplots()  # make the figure and axes for plotting
     a, b, c, d, w = calculate_coefficients(t, data)  # calculate the coefficients for the model
 
-    def f(t):
-        return a + b * t + c * np.cos(w * t) + d * np.sin(w * t)
+    def f(x):
+        return a + b * x + c * np.cos(w * x) + d * np.sin(w * x)
     t_smooth = np.linspace(1941, 2022.5, 50000)  # smooth time
     y = f(t_smooth)
     ax.plot(t, data, label="Actual", linewidth=0.2)  # plot the actual data
@@ -56,4 +57,3 @@ def plot_data(t, data, name):
 plot_data(Time, MaxTemp, "Max Temp")
 plot_data(Time, MinTemp, "Min Temp")
 plot_data(Time, SunHours, "Sun Hours")
-
